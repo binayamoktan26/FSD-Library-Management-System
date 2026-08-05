e23467d56305d4329940dbe973485edb4a795312
nawang tashi lama <binayamoktan26@gmail.com>
2026-08-05 23:09:18 +0545

    layout

---
+++ online_fe_lms/src/components/layouts/DefaultLayouts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DefaultLayouts = () => {
+  return (
+    <div>DefaultLayouts</div>
+  )
+}
+
+export default DefaultLayouts
--- online_fe_lms/src/pages/index.js
+++ online_fe_lms/src/pages/index.js
@@ -1,2 +1,4 @@
 export {default as HomePage} from "./home/HomePage";
 export {default as DashboardPage} from "./dashboard/DashboardPage";
+
+
