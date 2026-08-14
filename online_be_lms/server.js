f0ca5673d98c993674e89a631ddced04f1b3ecd2
nawang tashi lama <binayamoktan26@gmail.com>
2026-08-14 23:33:01 +0545

    basic server setup of Backend

---
+++ online_be_lms/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// middleware
+import cors from "cors";
+import morgan from "morgan";
+app.use(cors()); // it help to connect the frontend and backend
+app.use(morgan("dev")); // it help to see the request and response in the console
+app.use(express.json()); // it help to receive all the data send from the frontend as a body in json format
+
+// server status
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`Server is running at http://localhost:${PORT} `);
+});
