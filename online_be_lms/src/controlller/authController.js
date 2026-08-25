76f31983819d65decc76149838883442211194cb
nawang tashi lama <binayamoktan26@gmail.com>
2026-08-25 23:45:32 +0545

    storing user in database

--- online_be_lms/src/controlller/authController.js
+++ online_be_lms/src/controlller/authController.js
@@ -1,6 +1,14 @@
 export const insertNewUser = (req, res, error) => {
   try {
+    console.log(req.body);
+    const { password } = req.body;
+    
+
     //todo signUp process
+    // receive the user data
+
+    // encrypt the password
+    // insert user into DB
 
     res.json({
       status: "success",
