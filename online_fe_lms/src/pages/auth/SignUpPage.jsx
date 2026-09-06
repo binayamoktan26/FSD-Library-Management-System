f34de5a6c423e3831276ad7c2c157c29023d9991
nawang tashi lama <binayamoktan26@gmail.com>
2026-09-06 23:32:55 +0545

    signUP form

--- online_fe_lms/src/pages/auth/SignUpPage.jsx
+++ online_fe_lms/src/pages/auth/SignUpPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { CustomInput } from "../../components/customInput/CustomInput";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className=" d-flex justify-content-center align-items-center">
+      {" "}
+      <Form style={{ width: "450px" }} className="card p-5 mt-5 shadow">
+        <h3>Join Our Library Community</h3>
+        <CustomInput />
 
-export default SignUp
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default SignUp;
