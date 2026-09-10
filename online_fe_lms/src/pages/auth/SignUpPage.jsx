09675cf4cca16860f5772b1942efb1beba7e3653
nawang tashi lama <binayamoktan26@gmail.com>
2026-09-10 23:51:06 +0545

    creating custom hook-useForm

--- online_fe_lms/src/pages/auth/SignUpPage.jsx
+++ online_fe_lms/src/pages/auth/SignUpPage.jsx
@@ -3,15 +3,25 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { CustomInput } from "../../components/customInput/CustomInput";
 import { signUpInputs } from "../../assets/custominputs/userSignUpInputs.js";
-
+import useForm from "../../hooks/useForm.js";
+const initialState = {};
 const SignUp = () => {
+  const { form, setForm, handleOnChange } = useForm(initialState);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  console.log(form);
   return (
     <div className=" d-flex justify-content-center align-items-center">
       {" "}
-      <Form style={{ width: "450px" }} className="card p-5 mt-5 shadow">
+      <Form
+        onSubmit={handleOnSubmit}
+        style={{ width: "450px" }}
+        className="card p-5 m-5 shadow"
+      >
         <h3>Join Our Library Community</h3>
         {signUpInputs.map((input) => (
-          <CustomInput key={input.name} {...input} />
+          <CustomInput key={input.name} {...input} onChange={handleOnChange} />
         ))}
 
         <Button variant="primary" type="submit">
