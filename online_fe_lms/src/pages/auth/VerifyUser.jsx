fc827b5319acaa9f8302c72af500549d476f781b
nawang tashi lama <binayamoktan26@gmail.com>
2026-09-19 23:27:58 +0545

    user activation page done

--- online_fe_lms/src/pages/auth/VerifyUser.jsx
+++ online_fe_lms/src/pages/auth/VerifyUser.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
 const VerifyUser = () => {
+  const [isPending, setIsPending] = useState(true);
   return (
-    <div>VerifyUser</div>
-  )
-}
+    <div className="py-3">
+      {isPending && (
+        <div className="m-auto" style={{ width: "450px" }}>
+          <div className="d-flex justify-content-center">
+            {" "}
+            <Spinner animation="border" variant="primary" />
+          </div>
+          <div>Please do not go back or refresh the browser . Please wait </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default VerifyUser
+export default VerifyUser;
